4eb475ac4d3d56987e9d6d4b8c6d95a9f279167f
Mrjtjmn <meritamen@sdf.org>
2026-07-18 17:07:08 +0800

    Remove redundant brackets

--- src/Lisp/Eval.hs
+++ src/Lisp/Eval.hs
@@ -41,7 +41,7 @@ eval (LispList (LispSymbol "cond":clauses)) = do
 
 eval (LispSymbol varname) = getVar varname
 
-eval (LispList [LispSymbol "quote", (LispList (LispSymbol "lambda":params:body))]) = do
+eval (LispList [LispSymbol "quote", LispList (LispSymbol "lambda":params:body)]) = do
   envCtxRef <- ask
   params' <- handleParams params
   mkFn envCtxRef params' body
